4aac2fb39c707d24441b366b7cb459cffd97a2b0
Hugo Moslener <hugo.moslener@gmail.com>
2025-10-07 12:42:57 -0400

    Added sql statements in public methods so that they can be used

--- src/main/system/DatabaseReader.java
+++ src/main/system/DatabaseReader.java
@@ -1,22 +1,24 @@
 package main.system;
+
+import main.system.TrainRoute;
 import java.sql.*;
 import java.util.ArrayList;
 
 public class DatabaseReader {
-    public ArrayList<TrainRoutes> PullDataFromDatabase(){
+    static final String SCHEMA_NAME = "train_connections";
+    static final String TABLE_NAME = "eu_rail_network";
+    private ArrayList<TrainRoute> pullFromDatabase(String query){
 
-
-        String url = "jdbc:mysql://localhost:3306/testDatabase"; // Database details
+        String url = String.format("jdbc:mysql://localhost:3306/%s", SCHEMA_NAME); // Database details
         String username = "root"; // MySQL credentials
         String password = "Facile123";
-        String query = "SELECT * FROM testDatabase.eu_rail_network_ascii WHERE `Departure City` = 'Paris';"; // Query to be run
+        //String query = "SELECT * FROM testDatabase.eu_rail_network_ascii WHERE `Departure City` = 'Paris';"; // Query to be run
 
-        ArrayList<TrainRoutes> connections = new ArrayList<TrainRoutes>();
+        ArrayList<TrainRoute> connections = new ArrayList<TrainRoute>();
 
         try (Connection conn = DriverManager.getConnection(url, username, password);
              Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(query)) {
-
 
             while (rs.next()) {
                 String routeID = rs.getString(1);
@@ -29,14 +31,36 @@ public class DatabaseReader {
                 String firstClassTicketRate = rs.getString(8);
                 String secondClassTicketRate = rs.getString(9);
 
-                connections.add(new TrainRoutes(routeID, departureCity, arrivalCity, departureTime, arrivalTime, trainType, daysOfOperation, firstClassTicketRate, secondClassTicketRate));
+                connections.add(new TrainRoute(routeID, departureCity, arrivalCity, departureTime, arrivalTime, trainType, daysOfOperation, firstClassTicketRate, secondClassTicketRate));
+            }
+
+            if(connections.size() == 0){
+                // make a no returns thing
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return connections;
+    }
 
+    public ArrayList<TrainRoute> findArrivals(String arrivalCity){
+        String query = String.format("SELECT * FROM %s WHERE `Arrival City` = '%s'",TABLE_NAME, arrivalCity);
+        return pullFromDatabase(query);
+    }
+
+    public ArrayList<TrainRoute> findDepartures(String departureCity){
+        String query = String.format("SELECT * FROM %s WHERE `Departure City` = '%s'",TABLE_NAME, departureCity);
+        return pullFromDatabase(query);
+    }
+
+    public ArrayList<TrainRoute> findDepartureArrivalPair(String departureCity, String arrivalCity){
+        String query = String.format("SELECT * FROM %s WHERE `Departure City` = '%s' AND `Arrival City` = '%s'",TABLE_NAME, departureCity, arrivalCity);
+        return pullFromDatabase(query);
+    }
+
+    public ArrayList<TrainRoute> listAllCities(){
+        String query = String.format("SELECT `Departure City` AS City FROM %s UNION SELECT `Arrival City` AS City FROM %s;",TABLE_NAME, TABLE_NAME);
+        return pullFromDatabase(query);
     }
 }
