25f879d25728e9a4c51e6444f41f576d2096ac94
maelle <maelle.fbedard@gmail.com>
2025-10-21 13:17:21 -0400

    Attributes added for new classes

--- src/main/system/Ticket.java
+++ src/main/system/Ticket.java
@@ -1,4 +1,14 @@
 package main.system;
+import java.util.UUID;
 
 public class Ticket {
+    private UUID id;
+
+    public Ticket() {
+        UUID uuid = UUID.randomUUID();
+        this.id = uuid;
+    }
+
+
+
 }
--- src/main/system/Trip.java
+++ src/main/system/Trip.java
@@ -1,4 +1,15 @@
 package main.system;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class Trip {
+    private UUID tripId;
+    private List<Reservation>  reservations = new ArrayList<Reservation>();
+
+    public Trip() {
+        this.tripId = UUID.randomUUID();
+
+    }
+
 }
